ac90bffe0fd474af6c0f2a751b4ce90d4a045525
shlokadaga <60999620+shlokadaga@users.noreply.github.com>
2021-05-11 16:54:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import base64
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
